15a22bf2f699003e8c5c0c337fef715dcdf25e7f
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-18 00:00:00 +0000

    refactor: Add `perSystem` function and restructure outputs for multiple systems

--- outputs.nix
+++ outputs.nix
@@ -1,43 +1,58 @@
-{
-  self,
-  inputs,
-}: let
-  supportedSystems = ["x86_64-linux" "aarch64-linux"];
-
-  forAllSystems = lib.genAttrs supportedSystems;
-
-  lib = forAllSystems (system:
-    import ../lib {
-      inherit inputs system;
-      pkgs = pkgs.${system};
-    });
-
-  pkgs = forAllSystems (system:
-    import inputs.nixpkgs
-    {
-      inherit system;
-      inherit (lib.${system}.lib) overlays;
-      config.allowUnfree = true;
-    });
-
-  nixosModules.fuyu-no-kosei = {
-    imports = [
-      ./modules/nixos
-    ];
-  };
-
-  homeManagerModules.fuyu-no-kosei = {
-    imports = [
-      ./modules/home
-    ];
-  };
-
-  templates = {
-    starter = {
-      path = ./templates/starter;
-      description = "Fuyu-no-kosei starter template";
+{inputs}: let
+  perSystem = {
+    config,
+    lib,
+    pkgs,
+    self',
+    system,
+    ...
+  }: {
+    modules = {
+      nixos = import ./modules/nixos;
+      homeManager = import ./modules/homeManager;
     };
+    nixosModules = self'.modules.nixos;
+    homeManagerModules = self'.modules.homeManager;
+
+    lib = let
+      nixpkgsLib =
+        import inputs.nixpkgs
+        {
+          inherit system;
+        }
+        .lib;
+
+      fuyuNoKoseiLib =
+        import ./lib
+        {
+          inherit inputs system pkgs;
+        };
+    in
+      nixpkgsLib // fuyuNoKoseiLib;
+
+    pkgs = import inputs.nixpkgs {
+      inherit system;
+      inherit (lib) overlays;
+      config.allowUnfree = true;
+    };
+
+    templates = {
+      starter = {
+        path = ./templates/starter;
+        description = "Fuyu-no-kosei starter template";
+      };
+    };
+    defaultTemplate = self'.templates.starter;
   };
 
-  defaultTemplate = self.template.starter;
-in {inherit nixosModules homeManagerModules templates defaultTemplate lib;}
+  systems = ["x86_64-linux" "riscv64-linux" "aarch64-linux"];
+
+  systemOutputs = builtins.map (system: perSystem // {inherit system;}) systems;
+in {
+  nixosModules = builtins.map (o: o.nixosModules) systemOutputs;
+  homeManagerModules = builtins.map (o: o.homeManagerModules) systemOutputs;
+  templates = builtins.map (o: o.templates) systemOutputs;
+  defaultTemplate = builtins.map (o: o.defaultTemplate) systemOutputs;
+  lib = builtins.map (o: o.lib) systemOutputs;
+  pkgs = builtins.map (o: o.pkgs) systemOutputs;
+}
