216858778049525381e43639813c1c1ef8f682da
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-19 00:00:00 +0000

    Add inheritance to inputs attrSet passed to outputs.nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "NixOS configuration that supports multiple users, systems, and architectures.";
 
-  outputs = inputs: import ./outputs.nix inputs;
+  outputs = inputs: import ./outputs.nix {inherit inputs;};
 
   inputs = {
     /*
