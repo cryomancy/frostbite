4d3a3663bb1fdf66acea6a08ab5c01a79d633538
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-19 00:00:00 +0000

    temporarily remove templates

--- templates/starter/flake.nix
+++
@@ -1,9 +0,0 @@
-let
-  inputs.fuyuNoKosei.url = "github:TahlonBrahic/fuyu-no-kosei";
-in {
-  description = "NixOS configuration that follows fuyu-no-kosei.";
-
-  outputs = inputs: import ./outputs {inherit inputs;};
-
-  inherit (inputs.fuyuNoKosei.inputs) inputs;
-}
