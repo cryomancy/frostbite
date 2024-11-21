a90c454ee1ade4c0a7f180204568f2a77833129b
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-21 00:00:00 +0000

    add an argument to system template to allow defining custom options

--- lib/systemTemplate.nix
+++ lib/systemTemplate.nix
@@ -3,18 +3,20 @@
   pkgs,
   system,
   hostName,
+  fuyuConfig,
   users,
   lib,
   overlays,
 }: let
   nixosConfigurations = let
-    inherit (inputs) home-manager fuyuNoKosei;
+    inherit (inputs) home-manager homeManagerModules nixosModules;
     specialArgs = {inherit inputs system pkgs overlays users hostName;};
   in
     lib.nixosSystem {
       inherit system specialArgs;
       modules =
-        fuyuNoKosei.nixosModules.fuyuNoKosei
+        nixosModules.fuyuNoKosei
+        ++ fuyuConfig
         ++ [
           home-manager.nixosModules.home-manager
           {
@@ -25,7 +27,7 @@
               extraSpecialArgs = specialArgs;
               # Iterates over a list of users provided in the function call
               users = lib.attrsets.genAttrs users (user: {
-                imports = fuyuNoKosei.homeManagerModules.fuyuNoKosei;
+                imports = homeManagerModules.fuyuNoKosei;
                 config.home.username = user;
               });
             };
