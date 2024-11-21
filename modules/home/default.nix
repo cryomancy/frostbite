0769f4331937f70174d55692270c2cd3cad3fef1
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-21 00:00:00 +0000

    import modules in respective default.nix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,3 +1,16 @@
 {
-  imports = [];
+  imports = [
+    ./compositor.nix
+    ./design.nix
+    ./editor.nix
+    ./encryption.nix
+    ./homeEnvironment.nix
+    ./homeManagement.nix
+    ./homePackages.nix
+    ./menu.nix
+    ./passwordManagement.nix
+    ./shell.nix
+    ./status.nix
+    ./virtualization.nix
+  ];
 }
