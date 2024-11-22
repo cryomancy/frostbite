b98dc4f49401e3cb86c231a23c05c5c94d04300f
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-22 00:00:00 +0000

    push fuyuKosei in list scooe

--- lib/systemTemplate.nix
+++ lib/systemTemplate.nix
@@ -14,24 +14,23 @@
   in
     lib.nixosSystem {
       inherit system specialArgs;
-      modules =
+      modules = [
         nixosModules.fuyuNoKosei
-        ++ [
-          home-manager.nixosModules.home-manager
-          nixosModules.fuyuNoKosei
-          {
-            home-manager = {
-              inherit (lib) backupFileExtension;
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = specialArgs;
-              # Iterates over a list of users provided in the function call
-              users = lib.attrsets.genAttrs users (user: {
-                imports = homeManagerModules.fuyuNoKosei;
-                config.home.username = user;
-              });
-            };
-          }
-        ];
+        home-manager.nixosModules.home-manager
+        nixosModules.fuyuNoKosei
+        {
+          home-manager = {
+            inherit (lib) backupFileExtension;
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = specialArgs;
+            # Iterates over a list of users provided in the function call
+            users = lib.attrsets.genAttrs users (user: {
+              imports = homeManagerModules.fuyuNoKosei;
+              config.home.username = user;
+            });
+          };
+        }
+      ];
     };
 }
