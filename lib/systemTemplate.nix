9096693b66318e5014982e15ba2eeeadbbd97a97
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-26 00:00:00 +0000

    fix: remove unecessary nixosConfigurations from system templates that was preventing builds

--- lib/systemTemplate.nix
+++ lib/systemTemplate.nix
@@ -7,31 +7,29 @@
   users,
   lib,
   overlays,
-}: {
-  nixosConfigurations = let
-    inherit (inputs) home-manager homeManagerModules nixosModules;
-    specialArgs = {inherit inputs system pkgs overlays users hostName;};
-  in
-    lib.nixosSystem {
-      inherit system specialArgs;
-      modules =
-        [
-          nixosModules.fuyuNoKosei
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              backupFileExtension = "bak";
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = specialArgs;
-              # Iterates over a list of users provided in the function call
-              users = lib.attrsets.genAttrs users (user: {
-                imports = [homeManagerModules.fuyuNoKosei];
-                config.home.username = user;
-              });
-            };
-          }
-        ]
-        ++ fuyuConfig;
-    };
-}
+}: let
+  inherit (inputs) home-manager homeManagerModules nixosModules;
+  specialArgs = {inherit inputs system pkgs overlays users hostName;};
+in
+  lib.nixosSystem {
+    inherit system specialArgs;
+    modules =
+      [
+        nixosModules.fuyuNoKosei
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            backupFileExtension = "bak";
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = specialArgs;
+            # Iterates over a list of users provided in the function call
+            users = lib.attrsets.genAttrs users (user: {
+              imports = [homeManagerModules.fuyuNoKosei];
+              config.home.username = user;
+            });
+          };
+        }
+      ]
+      ++ fuyuConfig;
+  }
