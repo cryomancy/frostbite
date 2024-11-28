d79fd33e5535403282b441beba2e4f5deb89e4f3
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-27 20:37:09 -0600

    add git back to homeEnvironment

--- modules/home/homeEnvironment.nix
+++ modules/home/homeEnvironment.nix
@@ -74,6 +74,46 @@ in {
         enableFishIntegration = true;
         enableZshIntegration = true;
       };
+      git = {
+    enable = true;
+
+    lfs.enable = true;
+
+    delta.enable = true;
+    delta.options = {
+      line-numbers = true;
+      side-by-side = true;
+      navigate = true;
+    };
+
+    userName = "TahlonBrahic";
+    userEmail = "tahlonbrahic@gmail.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+
+      push = {
+        default = "current";
+        autoSetupRemote = true;
+      };
+
+      pull = {
+        rebase = true;
+      };
+
+      merge = {
+        conflictstyle = "diff3";
+      };
+
+      diff = {
+        colorMoved = "default";
+      };
+
+      url = {
+        "ssh://git@github.com/TahlonBrahic" = {
+          insteadOf = "https://github.com/TahlonBrahic";
+        };
+      };
       lsd = {
         enable = true;
         enableAliases = true;
