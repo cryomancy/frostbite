4c6c3fbc5b032ba26b4570ebc8029a9c7200a285
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-28 12:21:02 -0600

    fix: add package for chromium

--- modules/home/browser.nix
+++ modules/home/browser.nix
@@ -38,6 +38,7 @@ in {
   config = lib.mkIf cfg.enable {
     home.packages =
       lib.lists.optionals cfg.chrome.enable [pkgs.google-chrome]
+      ++ lib.lists.options cfg.chromium.enable [pkgs.ungoogled-chromium]
       ++ lib.lists.optionals cfg.terminal.enable
       [
         pkgs.w3m
@@ -322,8 +323,25 @@ in {
           id = 1;
           extensions = with pkgs.nur.repos.rycee.firefox-addons; [
             ublock-origin
-	    dark-mode-website-switcher
+            dark-mode-website-switcher
           ];
+          containers = {
+            jira = {
+              id = 1;
+            };
+            microsoft = {
+              id = 2;
+            };
+            vsphere = {
+              id = 3;
+            };
+            consoles = {
+              id = 4;
+            };
+            research = {
+              id = 5;
+            };
+          };
           containersForce = true;
           search = {
             force = true;
