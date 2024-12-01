9be66bce7b5d864b73aac74d083593a2e5c6958c
TahlonBrahic <tahlonbrahic@gmail.com>
2024-11-30 23:29:11 -0600

    fix: add missing programs prefix

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -12,36 +12,38 @@ in {
     };
   };
   config = lib.mkIf cfg.enable {
-    gamemode = {
-      enable = true;
-      settings = {
-        general = {
-          softrealtime = "auto";
-          renice = 15;
-        };
-        gpu = {
-          apply_gpu_optimizations = "accept-responsibility";
-          gpu_device = 0;
+    programs = {
+      gamemode = {
+        enable = true;
+        settings = {
+          general = {
+            softrealtime = "auto";
+            renice = 15;
+          };
+          gpu = {
+            apply_gpu_optimizations = "accept-responsibility";
+            gpu_device = 0;
+          };
         };
       };
-    };
-    steam = lib.mkIf {
-      enable = true;
+      steam = lib.mkIf {
+        enable = true;
 
-      package = pkgs.steam.override {
-        extraPkgs = pkgs:
-          with pkgs; [
-            keyutils
-            libkrb5
-            libpng
-            libpulseaudio
-            libvorbis
-            stdenv.cc.cc.lib
-            xorg.libXcursor
-            xorg.libXi
-            xorg.libXinerama
-            xorg.libXScrnSaver
-          ];
+        package = pkgs.steam.override {
+          extraPkgs = pkgs:
+            with pkgs; [
+              keyutils
+              libkrb5
+              libpng
+              libpulseaudio
+              libvorbis
+              stdenv.cc.cc.lib
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXinerama
+              xorg.libXScrnSaver
+            ];
+        };
       };
     };
     environment.systemPackages = with pkgs; [
