7a5ce826bdc3851e457013eddfc415eab7459fb9
TahlonBrahic <tahlonbrahic@gmail.com>
2024-12-01 00:00:00 +0000

    completely disable starship

--- modules/home/homeEnvironment.nix
+++ modules/home/homeEnvironment.nix
@@ -119,25 +119,25 @@ in {
         enableAliases = true;
       };
 
-      starship = {
-        enable = false;
-        enableFishIntegration = true;
-        settings = {
-          aws.disabled = true;
-          gcloud.disabled = true;
-          kubernetes.disabled = false;
-          git_branch.style = "242";
-          directory = {
-            style = "blue";
-            truncate_to_repo = false;
-            truncation_length = 8;
-          };
-          python.disabled = true;
-          ruby.disabled = true;
-          hostname.ssh_only = false;
-          hostname.style = "bold green";
-        };
-      };
+      #starship = {
+      #  enable = false;
+      #  enableFishIntegration = true;
+      #  settings = {
+      #    aws.disabled = true;
+      #    gcloud.disabled = true;
+      #    kubernetes.disabled = false;
+      #    git_branch.style = "242";
+      #    directory = {
+      #      style = "blue";
+      #      truncate_to_repo = false;
+      #      truncation_length = 8;
+      #    };
+      #    python.disabled = true;
+      #    ruby.disabled = true;
+      #    hostname.ssh_only = false;
+      #    hostname.style = "bold green";
+      #  };
+      #};
 
       zoxide = {
         enable = true;
