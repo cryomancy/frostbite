72945da4f9efed124ffeaeab2fdff5aba967c8b5
TahlonBrahic <tahlonbrahic@gmail.com>
2024-12-08 00:00:00 +0000

    Add dolphin file manager and plugins

--- modules/home/compositor.nix
+++ modules/home/compositor.nix
@@ -16,7 +16,7 @@ in {
     fuyuNoKosei.compositor = {
       enable = lib.mkOption {
         type = lib.types.bool;
-        default = true;
+        default = false;
       };
       hyprland = {
         enable = lib.mkEnableOption "hyprland";
---
+++ modules/home/fileManager.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.fuyuNoKosei.fileManager;
+in {
+  options = {
+    fuyuNoKosei.menu = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = config.fuyuNoKosei.compositor.enable;
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs.kdePackages; [
+      dolphin
+      dolphin-plugins
+      qtsvg
+      qtwayland
+      kio-fuse
+      kio-extras
+    ];
+  };
+}
