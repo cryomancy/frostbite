a2a0c382bc05200cba33d60977847982ef4b0059
TahlonBrahic <tahlonbrahic@gmail.com>
2024-12-18 00:00:00 +0000

    Add multiple applications to home Packages

--- modules/home/homePackages.nix
+++ modules/home/homePackages.nix
@@ -16,17 +16,49 @@ in {
   };
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
-      obsidian
-      audacity
-      gimp-with-plugins
-
+      # Torrenting
       bitmagnet
       xd
 
+      # Proton
       protonvpn-gui
       protonmail-desktop
 
-      obs-studio
+      # Music
+      musescore
+      audacity
+
+      # Visual
+      cheese
+      aseprite
+      inkscape
+      blender
+      gimp-with-plugins
     ];
+    programs = {
+      obs-studio = {
+        enable = true;
+        plugins = with pkgs.obs-studio-plugins; [
+          wlrobs
+          obs-vaapi
+          obs-nvfbc
+          obs-teleport
+          obs-vkcapture
+          obs-gstreamer
+          obs-3d-effect
+          input-overlay
+          obs-multi-rtmp
+          obs-source-clone
+          obs-shaderfilter
+          obs-source-record
+          obs-livesplit-one
+          looking-glass-obs
+          obs-command-source
+          obs-move-transition
+          obs-backgroundremoval
+          obs-pipewire-audio-capture
+        ];
+      };
+    };
   };
 }
