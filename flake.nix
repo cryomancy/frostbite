cbbcb1b1655a04c461571cea4098b2a4482efc09
TahlonBrahic <tahlonbrahic@gmail.com>
2024-12-19 00:00:00 +0000

    update nur.overlay to nur.overlays.default

--- flake.nix
+++ flake.nix
@@ -21,9 +21,6 @@
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-24.11";
     };
-    nixpkgs-unstable = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
-    };
     chaotic = {
       url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
     };
