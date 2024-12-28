63f4f55273aff1314142ff01d2b3cfbfea6487a7
TahlonBrahic <tahlonbrahic@gmail.com>
2024-12-28 00:00:00 +0000

    modify flake inputs

--- flake.nix
+++ flake.nix
@@ -54,94 +54,36 @@
     };
 
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-24.11";
-    };
+    base16.url = "github:SenchoPens/base16.nix";
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
+    disko.url = "github:nix-community/disko";
+    flake-compat.url = "github:edolstra/flake-compat";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    chaotic = {
-      url = "github:chaotic-cx/nyx/nyxpkgs-unstable";
-    };
-    nur = {
-      url = "github:nix-community/NUR";
-    };
-    fuyu-no-nur = {
-      url = "github:TahlonBrahic/fuyu-no-nur";
-    };
-    nix-index-database = {
-      url = "github:Mic92/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    impermanence = {
-      url = "github:nix-community/impermanence";
-    };
-    nixos-wsl = {
-      url = "github:nix-community/NixOS-WSL";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-24.05";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-      };
-    };
-    systems = {
-      url = "github:nix-systems/default-linux";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    haumea = {
-      url = "github:nix-community/haumea/v0.2.2";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    pre-commit-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix = {
-      url = "github:danth/stylix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
-      };
-    };
-    nixvim = {
-      url = "github:nix-community/nixvim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nvchad4nix = {
-      url = "github:nix-community/nix4nvchad";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    jeezyvim = {
-      url = "github:LGUG2Z/JeezyVim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    fuyuvim = {
-      url = "github:TahlonBrahic/fuyu-no-neovim";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    # Themeing
-    base16 = {
-      url = "github:SenchoPens/base16.nix";
-    };
-    tt-schemes = {
-      url = "github:tinted-theming/schemes";
-      flake = false;
-    };
-    walls = {
-      url = "github:dharmx/walls";
-      flake = false;
-    };
+    flake-utils.url = "github:numtide/flake-utils";
+    fuyu-no-nur.url = "github:TahlonBrahic/fuyu-no-nur";
+    fuyuvim.url = "github:TahlonBrahic/fuyu-no-neovim";
+    gitignore.url = "github:hercules-ci/gitignore.nix";
+    haumea.url = "github:nix-community/haumea/v0.2.2";
+    home-manager.url = "github:nix-community/home-manager";
+    impermanence.url = "github:nix-community/impermanence";
+    jeezyvim.url = "github:LGUG2Z/JeezyVim";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    nixvim.url = "github:nix-community/nixvim";
+    nmd.url = "gitlab:rycee/nmd";
+    nvchad4nix.url = "github:nix-community/nix4nvchad";
+    nix-index-database.url = "github:Mic92/nix-index-database";
+    nix-on-droid.url = "github:nix-community/nix-on-droid/release-24.05";
+    nur.url = "github:nix-community/NUR";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+    sops-nix.url = "github:Mic92/sops-nix";
+    systems.url = "github:nix-systems/default-linux";
+    stylix.url = "github:danth/stylix";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    ixx.url = "github:NuschtOS/ixx/v0.0.6";
+    tt-schemes.url = "github:tinted-theming/schemes";
+    tt-schemes.flake = false;
+    walls.url = "github:dharmx/walls";
+    walls.flake = false;
   };
 }
