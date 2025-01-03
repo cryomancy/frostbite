2d01e5e37e94c3554e78c6d6fc6bd4a05dcd3204
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-03 00:00:00 +0000

    simplify assets by importing custom flake assets

--- flake-parts/assets/flake.nix
+++ flake-parts/assets/flake.nix
@@ -2,15 +2,6 @@
   description = "A large repository of assets that are lazily evaluated.";
   inputs = {
     assets.url = "github:TahlonBrahic/assets";
-    base16.url = "github:SenchoPens/base16.nix";
-    tt-schemes = {
-      url = "github:tinted-theming/schemes";
-      flake = false;
-    };
-    walls = {
-      url = "github:dharmx/walls";
-      flake = false;
-    };
   };
 
   # This flake is only used for its inputs.
