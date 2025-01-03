d498f07f221d5fcfdcae69af539d0beb54a77968
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-02 21:00:33 -0600

    remove lib and overlays

--- flake-parts/misc/flake.nix
+++ flake-parts/misc/flake.nix
@@ -2,11 +2,9 @@
   inputs = {
     fuyu-no-nur.url = "github:TahlonBrahic/fuyu-no-nur";
     fuyuvim.url = "github:TahlonBrahic/fuyu-no-neovim";
-    haumea.url = "github:nix-community/haumea/v0.2.2";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     impermanence.url = "github:nix-community/impermanence";
     jeezyvim.url = "github:LGUG2Z/JeezyVim";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixvim.url = "github:nix-community/nixvim";
     nvchad4nix.url = "github:nix-community/nix4nvchad";
     nix-index-database.url = "github:Mic92/nix-index-database";
