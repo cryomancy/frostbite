73e9da3898a8f986db62299f85f265cd43b9afa1
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-02 19:36:56 -0600

    remove some inputs

--- flake.nix
+++ flake.nix
@@ -50,7 +50,6 @@
     flake-utils.url = "github:numtide/flake-utils";
     fuyu-no-nur.url = "github:TahlonBrahic/fuyu-no-nur";
     fuyuvim.url = "github:TahlonBrahic/fuyu-no-neovim";
-    gitignore.url = "github:hercules-ci/gitignore.nix";
     haumea.url = "github:nix-community/haumea/v0.2.2";
     home-manager.url = "github:nix-community/home-manager/release-24.11";
     impermanence.url = "github:nix-community/impermanence";
@@ -58,16 +57,12 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     nixvim.url = "github:nix-community/nixvim";
-    nmd.url = "sourcehut:~rycee/nmd";
     nvchad4nix.url = "github:nix-community/nix4nvchad";
     nix-index-database.url = "github:Mic92/nix-index-database";
-    nix-on-droid.url = "github:nix-community/nix-on-droid/release-24.05";
     nur.url = "github:nix-community/NUR";
     pre-commit-hooks.url = "github:cachix/git-hooks.nix";
     sops-nix.url = "github:Mic92/sops-nix";
     stylix.url = "github:danth/stylix";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
-    ixx.url = "github:NuschtOS/ixx/v0.0.6";
     tt-schemes.url = "github:tinted-theming/schemes";
     tt-schemes.flake = false;
     walls.url = "github:dharmx/walls";
