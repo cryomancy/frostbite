da9325e51414e668cb7ef976cf74d48cf6741d8f
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-03 21:51:02 -0600

    change lib loader

--- flake.nix
+++ flake.nix
@@ -60,7 +60,7 @@
           homeModules.fuyuNoKosei.homeModules = import ./flake-parts/modules/home;
           lib = inputs.haumea.lib.load {
             src = ./flake-parts/lib;
-            loader = inputs.haumea.lib.loader.callPackage;
+            loader = inputs.haumea.lib.loaders.callPackage;
             inputs = {inherit (inputs.nixpkgs) lib;};
           };
         };
