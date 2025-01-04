84c7edeedea4485963978fd0782c8f688f1671c8
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-03 21:59:29 -0600

    change systemTemplate function

--- flake-parts/lib/systemTemplate.nix
+++ flake-parts/lib/systemTemplate.nix
@@ -8,15 +8,14 @@
   lib,
   overlays,
 }: let
-  inherit (inputs) home-manager homeModules nixosModules;
   specialArgs = {inherit inputs system pkgs overlays users hostName;};
 in
   lib.nixosSystem {
     inherit system specialArgs;
     modules =
       [
-        nixosModules.fuyuNoKosei.nixosModules
-        home-manager.nixosModules.home-manager
+        inputs.nixosModules.fuyuNoKosei.nixosModules
+        inputs.home-manager.nixosModules.home-manager
         {
           home-manager = {
             backupFileExtension = "bak";
@@ -25,7 +24,7 @@ in
             extraSpecialArgs = specialArgs;
             # Iterates over a list of users provided in the function call
             users = lib.attrsets.genAttrs users (user: {
-              imports = [homeModules.fuyuNoKosei.homeModules];
+              imports = [inputs.homeModules.fuyuNoKosei.homeModules];
               config.home.username = user;
             });
           };
