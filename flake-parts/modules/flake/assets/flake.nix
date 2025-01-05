7f98ec76834f3062f5fcb19f8fa4956b2b4d9c8a
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-05 00:00:00 +0000

    remove unecessary modules

--- flake-parts/modules/flake/assets/flake.nix
+++
@@ -1,9 +0,0 @@
-localFlake: {
-  description = "A large repository of assets that are lazily evaluated.";
-  inputs = {
-    assets.url = "github:TahlonBrahic/assets";
-  };
-
-  outputs = {
-  };
-}
--- flake-parts/modules/flake/wsl/flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  description = "Inputs specific to NixOS on WSL.";
-  inputs = {
-    nixos-wsl.url = "github:nix-community/NixOS-WSL";
-  };
-
-  outputs = {
-  };
-}
