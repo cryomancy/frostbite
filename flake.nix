792dd4029540cf1562a151b3258d6f3adbdc6113
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-06 00:00:00 +0000

    add partitions

--- flake.nix
+++ flake.nix
@@ -3,61 +3,37 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/x86_64-linux";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
     haumea = {
       url = "github:nix-community/haumea/v0.2.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs @ {
-    flake-parts,
+  outputs = {
     self,
+    flake-utils,
+    haumea,
     ...
   }:
-    flake-parts.lib.mkFlake {inherit inputs self;} (
-      {
-        withSystem,
-        flake-parts-lib,
-        ...
-      }: let
-        inherit (flake-parts-lib) importApply;
-        flakeModules =
-          inputs.nixpkgs.lib.attrsets.genAttrs ["partitions"] (module:
-            importApply ./flake-parts/${module}/flake-module.nix {inherit withSystem;});
-        flakeModule = flakeModules;
-      in {
-        debug = true;
-        systems = ["x86_64-linux"];
-        imports =
-          [
-            inputs.flake-parts.flakeModules.partitions
-            inputs.flake-parts.flakeModules.flakeModules
-            inputs.flake-parts.flakeModules.modules
-            inputs.flake-parts.flakeModules.easyOverlay
-          ]
-          ++ inputs.nixpkgs.lib.attrsets.attrValues flakeModules;
-
-        flake = {
-          inherit flakeModule flakeModules;
-          # Traditional modules (seperate from the flake-parts system)
-          modules = {
-            nixos =
-              importApply
-              inputs.haumea.lib.load
-              {
-                src = ./flake-parts/modules/nixos;
-                loader = inputs.haumea.lib.loaders.verbatim;
-              };
-            home =
-              importApply
-              inputs.haumea.lib.load
-              {
-                src = ./flake-parts/modules/home;
-                loader = inputs.haumea.lib.loaders.verbatim;
-              };
+    flake-utils.lib.eachDefaultSystem (system: {
+      modules = {
+        nixos =
+          haumea.lib.load
+          {
+            src = ./partitions/modules/nixos;
+            loader = haumea.lib.loaders.verbatim;
           };
-        };
-      }
-    );
+        home =
+          haumea.lib.load
+          {
+            src = ./partitions/modules/home;
+            loader = haumea.lib.loaders.verbatim;
+          };
+      };
+    });
 }
