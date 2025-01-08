ecd57bc67738493b2220dbd5377b1f2d70d9d2f9
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-07 19:03:16 -0600

    testing inputs in build

--- flake.nix
+++ flake.nix
@@ -12,6 +12,9 @@
       url = "github:nix-community/haumea/v0.2.2";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    stylix = {
+      url = "github:danth/stylix/release-24.11";
+    };
   };
 
   outputs = {
