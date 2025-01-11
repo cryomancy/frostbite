87e1d0feeb556074a2a7b07009c1a1b4689bf2dc
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-11 11:04:11 -0600

    output lib and new systemTemplate

---
+++ lib/systemTemplate.nix
@@ -0,0 +1,41 @@
+scoped: {
+  self ? {},
+  extraModules ? [],
+  hostName ? "nixos",
+  inputs ? self.inputs,
+  lib ? inputs.nixpkgs.lib,
+  pkgs ? import inputs.nixpkgs {inherit system;},
+  system ? "x86_64-linux",
+  users ? ["nixos"],
+  ...
+}: let
+  inherit (inputs) kosei home-manager;
+  specialArgs = {inherit inputs hostName pkgs system users;};
+in
+  lib.nixosSystem {
+    inherit system specialArgs;
+    modules =
+      [
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            backupFileExtension = "bak";
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = specialArgs;
+            # Iterates over a list of users provided in the function call
+            users = inputs.nixpkgs.lib.attrsets.genAttrs users (user: {
+              imports =
+                lib.forEach
+                (builtins.attrNames kosei.modules.home)
+                (module: builtins.getAttr module kosei.modules.home);
+              config.home.username = user;
+            });
+          };
+        }
+      ]
+      ++ extraModules
+      ++ lib.forEach
+      (builtins.attrNames kosei.modules.nixos)
+      (module: builtins.getAttr module kosei.modules.nixos);
+  }
