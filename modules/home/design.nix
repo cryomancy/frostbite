2c2c4b70cb7ca1ba88c19d91b1fdc48bec0c1bfb
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-19 10:14:34 -0600

    force wallpaperd to build first

--- modules/home/design.nix
+++ modules/home/design.nix
@@ -20,7 +20,7 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    programs.wpaperd = {
+    programs.wpaperd = lib.mkForce {
       enable = true;
       settings = {
         path = cfg.wallpaper;
