17767911f9e7eb74c45c8eeb2a3c24bce7189606
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-20 00:00:00 +0000

    start docs for github pages

--- docs/flake.nix
+++ docs/flake.nix
@@ -1,20 +1,54 @@
 {
-  description = "Documentation for the Fuyu no Kosei module system";
-
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
 
-  outputs = {inputs}:
-    inputs.flake-utils.lib.eachDefaultSystem (
-      system: let
-        inherit (inputs.nixpkgs) lib;
-        pkgs = inputs.nixpkgs.legacyPackages.${system};
-      in {
-        packages = {
-          docs = import ./docs {inherit pkgs inputs lib;};
-        };
-      }
-    );
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    inherit
+      (nixpkgs.lib)
+      genAttrs
+      ;
+
+    eachSystem = f:
+      genAttrs
+      [
+        "aarch64-linux"
+        "x86_64-linux"
+      ]
+      (system: f nixpkgs.legacyPackages.${system});
+  in {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        packages = [pkgs.mdbook];
+        shellHook = ''
+          toplevel=$(git rev-parse --show-toplevel) || exit
+          cd "$toplevel" || exit
+          mkdir -p docs/theme
+          ln -sf ${pkgs.documentation-highlighter}/highlight.pack.js docs/theme/highlight.js
+          mdbook serve docs
+        '';
+      };
+    });
+
+    packages = eachSystem (pkgs: {
+      default = pkgs.stdenv.mkDerivation {
+        pname = "kosei-docs";
+        version = self.shortRev or "0000000";
+
+        src = ../.;
+
+        nativeBuildInputs = [pkgs.mdbook];
+
+        buildPhase = ''
+          cd docs
+          mkdir theme
+          ln -s ${pkgs.documentation-highlighter}/highlight.pack.js theme/highlight.js
+          mdbook build -d $out
+        '';
+      };
+    });
+  };
 }
---
+++ docs/src/api/home.md
@@ -0,0 +1,19 @@
+# `Home Manager Options`
+
+## Browser
+
+Kosei comes with five pre-configured browsers out of the box.
+These are `chrome`, `chromium`, `firefox`, `librewolf`, and `lynx`.
+The all plug into the design modules with Stylix thus integrating
+with the rest of the ecosystem. They also have a few preconfigured
+stylesheets to choose from to aid in furth customization. To enable
+any of the pre-configured browsers add this to extraModules:
+
+```nix
+{
+  home-manager.users."tahlon".kosei = {
+    browser.firefox.enable = true;
+  };
+}
+
+```
