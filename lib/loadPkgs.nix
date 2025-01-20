e2bcb69682ee137606fceeec3c677a57c3105abb
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-20 17:40:29 -0600

    add loadPkgs, loadConfigurations, and loadModulesRecursively functions in lib

---
+++ lib/loadPkgs.nix
@@ -0,0 +1,16 @@
+scoped: {
+  inputs,
+  system,
+}: {
+  pkgs =
+    import inputs.nixpkgs
+    {
+      inherit system;
+      config.allowUnfree = true;
+    }
+    .appendOverlays
+    [
+      inputs.fuyuvim.overlays.default
+      inputs.nur.overlays.default
+    ];
+}
