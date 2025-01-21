9d48bdb9c7d9d8380e997d0f63290cacda5744de
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-20 20:47:14 -0600

    update secrets to be a scoped path

--- modules/nixos/security/secrets.nix
+++ modules/nixos/security/secrets.nix
@@ -2,7 +2,7 @@ scoped: {
   config,
   inputs,
   lib,
-  pkgs,
+  self,
   users,
   ...
 }: let
@@ -18,7 +18,7 @@ in {
       };
       defaultSopsFile = lib.mkOption {
         type = lib.types.anything;
-        default = scoped: ./src/secrets/secrets.yaml;
+        default = "${self.outPath}/src/secrets/secrets.yaml";
       };
     };
   };
@@ -28,7 +28,7 @@ in {
       age = {
         keyFile = "/var/lib/sops-nix/key.txt";
       };
-      defaultSopsFile = cfg.defaultSopsFile "scoped";
+      inherit (cfg) defaultSopsFile;
       defaultSopsFormat = "yaml";
       secrets =
         lib.mergeAttrs
