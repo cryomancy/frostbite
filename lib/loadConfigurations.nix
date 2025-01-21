9c6efb4986616c11c3730df6b5c154365ab0202a
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-20 18:08:50 -0600

    define path on user side for loadConfigurations

--- lib/loadConfigurations.nix
+++ lib/loadConfigurations.nix
@@ -12,5 +12,5 @@ scoped: {
 in
   lib.attrsets.genAttrs systemNames
   (systemName:
-    import " ./ + ${src}${systemName}/${systemName}.nix"
+    import "./${src}/${systemName}/${systemName}.nix"
     {inherit inputs lib pkgs;})
