3c17ba9ec6c12d802934ca3fe1eb2e09a3e35b22
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-22 00:00:00 +0000

    inherit lib in customWaybarModule

--- lib/writeWaybarApplication.nix
+++ lib/writeWaybarApplication.nix
@@ -4,8 +4,10 @@ scoped: {
   dependencies ? [],
   lib ? {inherit lib;},
   pkgs ? {inherit pkgs;},
-}:
-lib.getExe (pkgs.writeShellApplication {
-  inherit name text;
-  runtimeInputs = with pkgs; [coreutils gnugrep systemd] ++ dependencies;
-})
+}: let
+  inherit (lib) getExe;
+in
+  getExe (pkgs.writeShellApplication {
+    inherit name text;
+    runtimeInputs = with pkgs; [coreutils gnugrep systemd] ++ dependencies;
+  })
