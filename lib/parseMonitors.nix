a2bc8af66da9634036e557ef6deb8a3de6108f8a
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-23 00:00:00 +0000

    change parseMonitors to writeShellApplication

--- lib/parseMonitors.nix
+++ lib/parseMonitors.nix
@@ -2,28 +2,32 @@ scoped: {
   lib,
   pkgs,
 }: let
-  parseMonitors = pkgs.writeShellScript "parseHyprctlMonitors" ''
-    monitors=$(hyprctl monitors all | grep Monitor | awk 'END {print NR}')
-    monitorNames=$(hyprctl monitors all | grep Monitor | awk '{print $2}')
-    resolutions=$(hyprctl monitors all | grep ' at ' | awk '{print $1}')
-    positions=$(hyprctl monitors all | grep ' at ' | awk '{print $3}')
-    scales=$(hyprctl monitors all | grep 'scale' | awk '{print $2}')
+  parseMonitors = lib.getExe (pkgs.writeShellApplication {
+    name = "parseMonitors";
+    runtimeInputs = with pkgs; [hyprland coreutils];
+    text = ''
+      monitors=$(hyprctl monitors all | grep Monitor | awk 'END {print NR}')
+      monitorNames=$(hyprctl monitors all | grep Monitor | awk '{print $2}')
+      resolutions=$(hyprctl monitors all | grep ' at ' | awk '{print $1}')
+      positions=$(hyprctl monitors all | grep ' at ' | awk '{print $3}')
+      scales=$(hyprctl monitors all | grep 'scale' | awk '{print $2}')
 
-    # Declare an array to hold monitor data
-    monitorData=()
+      # Declare an array to hold monitor data
+      monitorData=()
 
-    for i in $(seq 0 $(($monitors - 1))); do
-      # Assign the data for each monitor to a dictionary
-      monitorData[$i]=$(echo "$monitorNames $resolutions $positions $scales" | cut -d' ' -f$((i + 1)))
-    done
+      for i in $(seq 0 $(($monitors - 1))); do
+        # Assign the data for each monitor to a dictionary
+        monitorData[$i]=$(echo "$monitorNames $resolutions $positions $scales" | cut -d' ' -f$((i + 1)))
+      done
 
-    # Output the relevant data based on the parameter
-    if [ $1 == "count" ]; then
-      echo $monitors
-    else
-      echo monitorData[$1][$2]
-    fi
-  '';
+      # Output the relevant data based on the parameter
+      if [ $1 == "count" ]; then
+        echo $monitors
+      else
+        echo monitorData[$1][$2]
+      fi
+    '';
+  });
 
   parsedMonitors = lib.attrsets.mergeAttrsList (
     lib.forEach (builtins.genList (x: x + 1) (parseMonitors "count"))
