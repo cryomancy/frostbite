d69d6ca0041eb93d549ddf9dc32b6a5e5714d682
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-26 21:50:41 -0600

    pass arguments in list to builtins.exec

--- lib/parseMonitors.nix
+++ lib/parseMonitors.nix
@@ -3,34 +3,30 @@ scoped: {
   pkgs,
 }: let
   parseMonitors = arg: index:
-    pkgs.writeShellApplication {
-      name = "parseMonitors.sh";
-      runtimeInputs = with pkgs; [hyprland coreutils];
-      excludeShellChecks = ["SC2034" "SC2050"];
-      text = ''
-        monitors=$(hyprctl monitors all | grep Monitor | awk 'END {print NR}')
-        monitorNames=$(hyprctl monitors all | grep Monitor | awk '{print $2}')
-        resolutions=$(hyprctl monitors all | grep ' at ' | awk '{print $1}')
-        positions=$(hyprctl monitors all | grep ' at ' | awk '{print $3}')
-        scales=$(hyprctl monitors all | grep 'scale' | awk '{print $2}')
-        monitorData=()
+    pkgs.writeShellScript "parseMonitors.sh"
+    ''
+      monitors=$(hyprctl monitors all | grep Monitor | awk 'END {print NR}')
+      monitorNames=$(hyprctl monitors all | grep Monitor | awk '{print $2}')
+      resolutions=$(hyprctl monitors all | grep ' at ' | awk '{print $1}')
+      positions=$(hyprctl monitors all | grep ' at ' | awk '{print $3}')
+      scales=$(hyprctl monitors all | grep 'scale' | awk '{print $2}')
+      monitorData=()
 
-        for i in $(seq 0 "$monitors" - 1); do
-          monitorData[i]=$(echo "$monitorNames $resolutions $positions $scales" | cut -d' ' -f"$(i + 1)")
-        done
+      for i in $(seq 0 "$monitors" - 1); do
+        monitorData[i]=$(echo "$monitorNames $resolutions $positions $scales" | cut -d' ' -f"$(i + 1)")
+      done
 
-        if [ "${arg}" == "count" ]; then
-          echo "$monitors"
-        else
-          echo monitorData["${arg}"]["${index}"]
-        fi
-      '';
-    };
+      if [ "${arg}" == "count" ]; then
+        echo "$monitors"
+      else
+        echo monitorData["${arg}"]["${index}"]
+      fi
+    '';
 in
   lib.attrsets.mergeAttrsList (
     lib.forEach
     (builtins.genList (x: x)
-      (builtins.exec [parseMonitors "count" ""]))
+      (builtins.exec [(parseMonitors "count" "")]))
     (
       monitorIndex: {
         name = parseMonitors "name" monitorIndex;
