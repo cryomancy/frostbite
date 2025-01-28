f852fce7d40321b92858f5ee3af40edcd82b2be7
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-28 06:38:09 -0600

    modify parseMonitors to be purely functional

--- lib/parseMonitors.nix
+++ lib/parseMonitors.nix
@@ -6,26 +6,58 @@ let
     name = "parseMonitors.sh";
 	executable = true;
     text = ''
-      monitors=$(hyprctl monitors all | grep Monitor | awk 'END {print NR}')
-      monitorNames=$(hyprctl monitors all | grep Monitor | awk '{print $2}')
-      resolutions=$(hyprctl monitors all | grep ' at ' | awk '{print $1}')
-      positions=$(hyprctl monitors all | grep ' at ' | awk '{print $3}')
-      scales=$(hyprctl monitors all | grep 'scale' | awk '{print $2}')
-      monitorData=()
+		#!/usr/bin/env bash
 
-      for i in $(seq 0 "$monitors"); do
-        monitorData[i]=$(echo "$monitorNames $resolutions $positions $scales" | cut -d' ' -f"$(i + 1)")
-      done
+		monitors=$(hyprctl monitors all | grep Monitor | awk 'END {print NR}')
+		monitorNames=$(hyprctl monitors all | grep Monitor | awk '{print $2}')
+		resolutions=$(hyprctl monitors all | grep ' at ' | awk '{print $1}')
+		positions=$(hyprctl monitors all | grep ' at ' | awk '{print $3}')
+		scales=$(hyprctl monitors all | grep 'scale' | awk '{print $2}')
 
-      if [ "$1" == "count" ]; then
-        echo "$monitors"
-      else
-        echo monitorData["$1"]["$2"]
-      fi
-    '';
+		get_monitor_data() {
+			local monitor_index=$1
+			local attribute=$2
+
+			case "$attribute" in
+				name)
+					echo "${monitorNames[$monitor_index]}"
+					;;
+				resolution)
+					echo "${resolutions[$monitor_index]}"
+					;;
+				position)
+					echo "${positions[$monitor_index]}"
+					;;
+				scale)
+					echo "${scales[$monitor_index]}"
+					;;
+				*)
+					echo "Invalid attribute: $attribute"
+					exit 1
+					;;
+			esac
+		}
+
+		handle_case() {
+			local action=$1
+			if [ "$action" == "count" ]; then
+				# Return the count of monitors
+				echo "$monitors"
+			elif [ $# -eq 2 ]; then
+				# If 2 arguments are provided (monitor index and attribute), return the requested attribute
+				local monitor_index=$1
+				local attribute=$2
+				get_monitor_data "$monitor_index" "$attribute"
+			else
+				# Handle invalid arguments
+				echo "Usage: $0 {count | monitor_index attribute}"
+				exit 1
+			fi
+		}
+			'';
   } |> builtins.readFile;
 
-  callAttr = attr: monitorIndex:  builtins.exec ["bash" "-c" "${parseMonitors}" "--" "${attr}" "${monitorIndex}"];
+  callAttr = attr: monitorIndex:  builtins.exec ["bash" "-c" "${parseMonitors}" "--" "${monitorIndex}" "${attr}"];
 
   monitorCount = builtins.exec ["bash" "-c" "${parseMonitors}" "--" "count"];
 
