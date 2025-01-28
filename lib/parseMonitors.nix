86509f02bbd8dfce89ae99a1cdfb1412ec3832ea
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-27 21:12:18 -0600

    experimenting with builtins.exec

--- lib/parseMonitors.nix
+++ lib/parseMonitors.nix
@@ -35,7 +35,7 @@ scoped: {
     };
   };
   
-  monitors = builtins.exec ["${pkgs.bash}" "-c" "${parseMonitors}" "--" "count"]
+  monitors = builtins.exec ["${pkgs.bash}/bin/bash" "-c" "${parseMonitors}" "--" "count"]
     |> builtins.genList (x: x)
 	|> builtins.map formattedOutput
     |> lib.attrsets.mergeAttrsList;
