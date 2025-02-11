2eeb64ccfd584f86ce0da30b465d9f30e3f9648d
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-10 21:21:29 -0600

    update firefox config to include new extensions and a work profile

--- modules/home/browser/firefox.nix
+++ modules/home/browser/firefox.nix
@@ -8,12 +8,108 @@ scoped: {
 }: let
   cfg = config.kosei.browser.firefox;
   inherit (inputs.nur.legacyPackages.${system}.repos.rycee) firefox-addons;
+  userChrome = cfg.stylesheet;
+  settings = {
+    "app.normandy.api_url" = "";
+    "app.normandy.enabled" = false;
+    "app.shield.optoutstudies.enabled" = false;
+    "browser.aboutConfig.showWarning" = false;
+    "browser.bookmarks.addedImportButton" = false;
+    "browser.cache.disk.enable" = false;
+    "browser.cache.memory.capacity" = -1;
+    "browser.cache.memory.enable" = true;
+    "browser.contentblocking.report.lockwise.enabled" = false;
+    "browser.discovery.enabled" = false;
+    "browser.download.useDownloadDir" = false;
+    "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons" = false;
+    "browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features" = false;
+    "browser.newtabpage.activity-stream.feeds.discoverystreamfeed" = false;
+    "browser.newtabpage.activity-stream.feeds.section.topstories" = false;
+    "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+    "browser.newtabpage.activity-stream.showSponsored" = false;
+    "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+    "browser.newtabpage.activity-stream.section.highlights.includePocket" = false;
+    "browser.ping-centre.telemetry" = false;
+    "browser.preferences.defaultPerformanceSettings.enabled" = false;
+    "browser.search.suggest.enabled" = false;
+    "browser.startup.homepage" = "about:blank";
+    "browser.tabs.crashReporting.sendReport" = false;
+    "browser.toolbars.bookmarks.visibility" = "never";
+    "browser.urlbar.quickactions.enabled" = false;
+    "browser.urlbar.quickactions.showPrefs" = false;
+    "browser.urlbar.shortcuts.bookmarks" = false;
+    "browser.urlbar.shortcuts.history" = false;
+    "browser.urlbar.shortcuts.tabs" = false;
+    "browser.urlbar.suggest.bookmark" = false;
+    "browser.urlbar.suggest.engines" = false;
+    "browser.urlbar.suggest.history" = true;
+    "browser.urlbar.suggest.openpage" = false;
+    "browser.urlbar.suggest.searches" = false;
+    "browser.urlbar.suggest.topsites" = false;
+    "browser.uitour.enabled" = false;
+    "devtools.onboarding.telemetry.logged" = false;
+    "datareporting.healthreport.uploadEnabled" = false;
+    "datareporting.policy.dataSubmissionEnabled" = false;
+    "dom.security.https_first" = true;
+    "extensions.abuseReport.enabled" = false;
+    "extensions.formautofill.creditCards.enabled" = false;
+    "general.smoothScroll" = true;
+    "general.smoothScroll.lines.durationMaxMS" = 125;
+    "general.smoothScroll.lines.durationMinMS" = 125;
+    "general.smoothScroll.mouseWheel.durationMaxMS" = 200;
+    "general.smoothScroll.mouseWheel.durationMinMS" = 100;
+    "general.smoothScroll.msdPhysics.enabled" = true;
+    "general.smoothScroll.other.durationMaxMS" = 125;
+    "general.smoothScroll.other.durationMinMS" = 125;
+    "general.smoothScroll.pages.durationMaxMS" = 125;
+    "general.smoothScroll.pages.durationMinMS" = 125;
+    "identity.fxaccounts.commands.enabled" = false;
+    "identity.fxaccounts.enabled" = false;
+    "identity.fxaccounts.pairing.enabled" = false;
+    "identity.fxaccounts.toolbar.enabled" = false;
+    "middlemouse.paste" = false;
+    "mousewheel.min_line_scroll_amount" = 30;
+    "mousewheel.system_scroll_override_on_root_content.enabled" = true;
+    "mousewheel.system_scroll_override_on_root_content.horizontal.factor" = 175;
+    "mousewheel.system_scroll_override_on_root_content.vertical.factor" = 175;
+    "network.dns.disableIPv6" = true;
+    "network.dns.disablePrefetch" = true;
+    "network.prefetch-next" = false;
+    "network.proxy.socks_remote_dns" = true;
+    "permissions.default.camera" = 0;
+    "permissions.default.desktop-notification" = 2;
+    "permissions.default.geo" = 0;
+    "permissions.default.microphone" = 0;
+    "permissions.default.xr" = 2;
+    "pdfjs.enableScripting" = false;
+    "security.ssl.require_safe_negotiation" = true;
+    "signon.autofillForms" = false;
+    "signon.rememberSignons" = false;
+    "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+    "toolkit.scrollbox.horizontalScrollDistance" = 6;
+    "toolkit.scrollbox.verticalScrollDistance" = 2;
+    "toolkit.telemetry.archive.enabled" = false;
+    "toolkit.telemetry.bhrPing.enabled" = false;
+    "toolkit.telemetry.enabled" = false;
+    "toolkit.telemetry.firstShutdownPing.enabled" = false;
+    "toolkit.telemetry.newProfilePing.enabled" = false;
+    "toolkit.telemetry.server" = "data:,";
+    "toolkit.telemetry.shutdownPingSender.enabled" = false;
+    "toolkit.telemetry.updatePing.enabled" = false;
+    "toolkit.telemetry.unified" = false;
+  };
 in {
   options = {
     kosei.browser = {
-      firefox.enable = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
+      firefox = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+        };
+        stylesheet = lib.mkOption {
+          type = lib.types.str;
+          default = builtins.readFile ./_assets/twilly.css;
+        };
       };
     };
   };
@@ -26,29 +122,15 @@ in {
         main = {
           id = 0;
           name = "main";
-          settings = {
-            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-            "browser.urlbar.quickactions.enabled" = false;
-            "browser.urlbar.quickactions.showPrefs" = false;
-            "browser.urlbar.shortcuts.quickactions" = false;
-            "browser.urlbar.suggest.quickactions" = false;
-            "browser.startup.homepage" = "about:blank";
-            # TODO: add if Yubikey module enabled
-            # Yubikey
-            "security.webauth.u2f" = true;
-            "security.webauth.webauthn" = true;
-            "security.webauth.webauthn_enable_softtoken" = true;
-            "security.webauth.webauthn_enable_usbtoken" = true;
-            "distribution.searchplugins.defaultLocale" = "en-US";
-          };
-
-          # TODO: Add an option to choose styles?
-          userChrome = builtins.readFile ./_assets/twilly.css;
-
+          inherit settings userChrome;
           extensions = with firefox-addons; [
             ublock-origin
             simple-tab-groups
             darkreader
+            keepassxc-browser
+            musescore-downloader
+            sponsorblock
+            torrent-control
           ];
 
           search = {
@@ -56,6 +138,25 @@ in {
             order = [
               "DuckDuckGo"
               "Google"
+            ];
+          };
+        };
+
+        work = {
+          id = 1;
+          name = "work";
+          inherit settings userChrome;
+          extensions = with firefox-addons; [
+            ublock-origin
+            simple-tab-groups
+            darkreader
+            keepassxc-browser
+          ];
+          search = {
+            force = true;
+            order = [
+              "Google"
+              "DuckDuckGo"
             ];
           };
         };
