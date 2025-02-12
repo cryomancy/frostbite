b7fc25db0a53ec3a64f104b7780dbc02a48bf1e4
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-12 00:00:00 +0000

    add function to build live ISO

---
+++ lib/liveISO.nix
@@ -0,0 +1,42 @@
+scoped: {
+  extraModules ? [],
+  hostName ? "nixos",
+  inputs ? {},
+  lib ? inputs.nixpkgs.lib,
+  pkgs ? import inputs.nixpkgs {inherit system;},
+  outPath,
+  system ? "x86_64-linux",
+  users ? ["nixos"],
+  ...
+}: let
+  inherit (inputs) kosei home-manager nixpkgs;
+  specialArgs = {inherit hostName inputs outPath system users;};
+in
+  lib.nixosSystem {
+    inherit system specialArgs;
+    modules =
+      [
+        (nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            backupFileExtension = "bak";
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            extraSpecialArgs = specialArgs // {inherit pkgs;};
+            # Iterates over a list of users provided in the function call
+            users = inputs.nixpkgs.lib.attrsets.genAttrs users (user: {
+              imports =
+                lib.forEach
+                (builtins.attrNames kosei.modules.home)
+                (module: builtins.getAttr module kosei.modules.home);
+              config.home.username = user;
+            });
+          };
+        }
+      ]
+      ++ extraModules
+      ++ lib.forEach
+      (builtins.attrNames kosei.modules.nixos)
+      (module: builtins.getAttr module kosei.modules.nixos);
+  }
