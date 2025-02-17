14eebb69b489fb00f211ec6c726c2276774370af
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-17 00:00:00 +0000

    add gpg module to home modules

---
+++ modules/home/development/gpg.nix
@@ -0,0 +1,34 @@
+scoped: {
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  cfg = config.kosei.gpg;
+in {
+  options = {
+    kosei.gpg = {
+      enable = lib.mkOption {
+        default = true;
+        example = false;
+        description = "gpg";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = with pkgs; [
+      gpg-tui # Terminal user interface for GnuPG
+      gpgme # Library for making GnuPG easier to use
+    ];
+    programs = {
+      gpg = {
+        enable = true;
+        homedir = "${config.xdg.configHome}/gnupg";
+        enableBrowserSocket = true;
+        enableExtraSocket = true;
+        enableSSHSupport = true;
+      };
+    };
+  };
+}
