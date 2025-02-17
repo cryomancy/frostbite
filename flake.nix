aee128180b20d071649a4aaa94c73611b1daa01b
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-17 16:54:13 -0600

    add principal inputs in kosei to be imported to subordinate flakes

--- flake.nix
+++ flake.nix
@@ -1,12 +1,6 @@
 {
   description = "Nix flakes abstraction layer that supports multiple users, systems, and architectures.";
 
-  inputs = {
-    eris.url = "github:TahlonBrahic/eris";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-  };
-
   outputs = inputs @ {
     eris,
     flake-parts,
@@ -80,4 +74,79 @@
         };
       };
     });
+
+  inputs = {
+    assets = {
+      url = "github:TahlonBrahic/assets";
+      flake = false;
+    };
+    kosei = {
+      url = "github:TahlonBrahic/fuyu-no-kosei";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+    fuyu-no-nur = {
+      url = "github:TahlonBrahic/fuyu-no-nur";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    fuyuvim = {
+      url = "github:TahlonBrahic/fuyu-no-neovim";
+    };
+    eris = {
+      url = "github:TahlonBrahic/eris";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    impermanence = {
+      url = "github:nix-community/impermanence";
+    };
+    jeezyvim = {
+      url = "github:LGUG2Z/JeezyVim";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:/nixos/nixpkgs/nixos-24.11";
+    nixpkgs-master.url = "github:/nixos/nixpkgs";
+    nix-index-database = {
+      url = "github:Mic92/nix-index-database";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+    stylix = {
+      url = "github:Flameopathic/stylix/optional-image";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
 }
