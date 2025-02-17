2f17e351744945160dd9240910659ab8d4c235b8
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-17 00:00:00 +0000

    temp disable obs

--- modules/home/misc/homePackages.nix
+++ modules/home/misc/homePackages.nix
@@ -36,7 +36,7 @@ in {
 
     programs = {
       obs-studio = {
-        enable = true;
+        enable = false;
         plugins = with pkgs.obs-studio-plugins; [
           wlrobs
           obs-vaapi
