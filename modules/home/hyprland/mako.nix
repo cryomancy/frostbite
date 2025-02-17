bde897d1c0f1b3e8077c7344f6037e20bd893188
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-17 00:00:00 +0000

    move mako pkg to mako service

---
+++ modules/home/hyprland/mako.nix
@@ -0,0 +1,45 @@
+scoped: {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.kosei.mako;
+in {
+  options = {
+    kosei.mako = {
+      enable = lib.mkOption {
+        type = lib.types.bool;
+        default = config.kosei.hyprland.enable;
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [pkgs.libnotify];
+    services.mako = {
+      enable = true;
+
+      anchor = "top-right";
+      borderRadius = 5;
+      borderSize = 2;
+      padding = "20";
+      defaultTimeout = 5000;
+      layer = "top";
+      height = 100;
+      width = 300;
+      format = "<b>%s</b>\\n%b";
+
+      extraConfig = ''
+        [urgency=low]
+        default-timeout=3000
+
+        [urgency=high]
+        default-timeout=10000
+
+        [mode=dnd]
+        invisible=1
+      '';
+    };
+  };
+}
