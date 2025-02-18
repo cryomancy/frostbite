e53ca6da32950ae8b36026d37acb84d1a7e4965b
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-17 20:19:32 -0600

    start hypridle module

--- modules/home/hyprland/hypridle.nix
+++ modules/home/hyprland/hypridle.nix
@@ -1,6 +1,7 @@
 scoped: {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.kosei.hypridle;
@@ -17,6 +18,25 @@ in {
   config = lib.mkIf cfg.enable {
     services.hypridle = {
       enable = true;
+      settings = {
+        general = {
+          before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
+          lock_cmd = "hyprlock";
+        };
+
+        listener = [
+          {
+            timeout = 300;
+            on-timeout = "hyprlock";
+          }
+          {
+            timeout = 350;
+            on-timeout = "hyprctl dispatch dpms off";
+            on-resume = "hyprctl dispatch dpms on";
+          }
+        ];
+      };
     };
+    systemd.user.services.hypridle.Unit.After = lib.mkForce "graphical-session.target";
   };
 }
