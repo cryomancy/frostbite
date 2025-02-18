4603f650439dccc7326698d10a0454042e9cf4a3
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-18 00:00:00 +0000

    update default path in secrets module

--- modules/nixos/security/secrets.nix
+++ modules/nixos/security/secrets.nix
@@ -19,7 +19,7 @@ in {
       };
       defaultSopsFile = lib.mkOption {
         type = lib.types.path;
-        default = "${outPath}/secrets/secrets.yaml";
+        default = "${outPath}/src/secrets/secrets.yaml";
       };
     };
   };
