d64fab075b709a71c635d93105325dcee1e92657
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-17 21:17:59 -0600

    add nixos defualtServices module

---
+++ modules/nixos/services/defaultServices.nix
@@ -0,0 +1,24 @@
+scoped: {
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.kosei.defaultServices;
+in {
+  options = {
+    kosei.defaultServices = {
+      enable = lib.mkEnableOption "defaultServices";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services = {
+      earlyoom = {
+        enable = true;
+        enableNotifications = true;
+      };
+      gvfs.enable = true;
+      tumbler.enable = true;
+    };
+  };
+}
