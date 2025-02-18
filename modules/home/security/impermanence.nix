349c63275d788ccb818f87f4c5f8f2b37ffad093
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-17 21:19:54 -0600

    create blank slate for home impermanence module that stores under nixos impermance module

--- modules/home/security/impermanence.nix
+++ modules/home/security/impermanence.nix
@@ -2,7 +2,7 @@ scoped: {
   config,
   inputs,
   lib,
-  username,
+  user,
   ...
 }: let
   cfg = config.kosei.impermanence;
@@ -15,62 +15,13 @@ in {
 
   config = lib.mkIf cfg.enable {
     home.persistence = {
-      "/persist/home/${username}" = {
+      "/nix/persistent/home/${user}" = {
         allowOther = true;
 
         directories = [
-          "go"
-          ".docker"
-          ".emacs.d"
-          ".flutter-devtools"
-          ".kube"
-          ".m2"
-          ".mozilla"
-          ".librewolf"
-          ".thunderbird"
-          ".obsidian"
-          ".openvpn"
-          ".password-store"
-          ".themes"
-          ".terraform.d"
-          ".yandex"
-          ".ollama"
-          ".config/google-chrome"
-          ".config/sops"
-          ".config/vesktop"
-          ".config/sops-nix"
-          ".config/obsidian"
-          ".config/Code"
-          ".config/dconf"
-          ".config/htop"
-          ".config/nvim"
-          ".config/syncthing"
-          ".config/obs-studio"
-          ".config/pulse"
-          ".local/share/chat.fluffy.fluffychat"
-          ".local/share/zoxide"
-          ".local/share/fish"
-          ".local/share/nix"
-          ".local/share/containers"
-          ".local/share/Trash"
-          ".local/share/TelegramDesktop"
-          ".local/share/keyrings"
-          ".local/share/nvim"
-          ".local/state"
-          ".vscode"
-          ".pki"
-          ".ssh"
-          ".gnupg"
         ];
 
         files = [
-          ".bash_history"
-          ".bash_logout"
-          ".flutter"
-          ".face"
-          ".face.icon"
-          ".zsh_history"
-          ".cache/cliphist/db"
         ];
       };
     };
