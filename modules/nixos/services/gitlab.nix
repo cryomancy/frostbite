8e382153281f16ab891b798a1376df6a6601f577
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-17 21:14:28 -0600

    add service and gitlab-runner service to gitlab module

--- modules/nixos/services/gitlab.nix
+++ modules/nixos/services/gitlab.nix
@@ -1,1 +1,56 @@
-scoped: {}
+scoped: {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.kosei.gitlab;
+  systemStateVersion = config.system.stateVersion;
+in {
+  options = {
+    kosei.gitlab = {
+      enable = lib.mkEnableOption "gitlab";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      gitlab-shell
+    ];
+
+    # TODO: Asserts secrets must be enabled
+    containers = {
+      gitlab-container = {
+        autoStart = true;
+        config = {pkgs, ...}: {
+          services.gitlab = {
+            enable = true;
+            pages.enable = true;
+            packages.gitlab = pkgs.gitlab;
+            port = 3080;
+            smtp = {
+              enable = false;
+              port = 25;
+            };
+          };
+
+          services.gitlab-runner = {
+            enable = true;
+            settings = {
+            };
+          };
+
+          networking.firewall = {
+            allowedTCPPorts = [3080];
+            allowedTCPPortRanges = [
+              {
+                from = 6980;
+                to = 6990;
+              }
+            ];
+          };
+          system.stateVersion = systemStateVersion;
+        };
+      };
+    };
+  };
+}
