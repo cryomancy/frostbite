371ddaefdc5e1d6af91a5044a1e3c6cf8e27cd0b
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-19 00:00:00 +0000

    add impermanence statement to btrbk service container and add statement to create directory as btrbk doesn't create it

--- modules/nixos/services/btrbk.nix
+++ modules/nixos/services/btrbk.nix
@@ -5,6 +5,7 @@ scoped: {
   ...
 }: let
   cfg = config.kosei.btrbk;
+  systemStateVersion = config.system.stateVersion;
 in {
   options = {
     kosei.btrbk = {
@@ -12,13 +13,14 @@ in {
     };
   };
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-    ];
+    environment.persistence = lib.mkIf config.kosei.impermanence.enable {
+      "/nix/persistent/".directories = ["/var/snapshots"];
+    };
 
     containers = {
       btrbk-container = {
         autoStart = true;
-        config = {...}: {
+        config = _: {
           services.btrbk = {
             instances."btrbak" = {
               onCalendar = "*-*-* *:03:00";
@@ -28,19 +30,20 @@ in {
                 preserve_day_of_week = "sunday";
                 preserve_hour_of_day = "0";
                 target_preserve = "48h 10d 4w 12m 10y";
-                volume."/home" = {
-                  snapshot_create = "always";
-                  subvolume = ".";
-                  snapshot_dir = ".snapshots";
-                };
-                volume."/var/local" = {
-                  snapshot_create = "always";
-                  subvolume = ".";
-                  snapshot_dir = ".snapshots";
+                volume = {
+                  "/" = {
+                    snapshot_dir = "/var/snapshots";
+                    subvolume = "home";
+                  };
                 };
               };
             };
           };
+          systemd.tmpfiles.rules = [
+            "d /var/snapshots 0755 root root"
+          ];
+
+          system.stateVersion = systemStateVersion;
         };
       };
     };
