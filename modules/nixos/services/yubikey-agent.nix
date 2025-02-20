d7631df1574ec5e6fd03956619ecd330da77c831
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-20 06:35:21 -0600

    create yubikey-agent systemd service module

---
+++ modules/nixos/services/yubikey-agent.nix
@@ -0,0 +1,45 @@
+scoped: {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.kosei.yubikey-agent;
+in {
+  options = {
+    kosei.yubikey-agent = {
+      enable = mkOption {
+        type = types.bool;
+        default = config.kosei.yubikey.enable;
+        description = ''
+          Whether to start yubikey-agent when you log in.
+          Also sets SSH_AUTH_SOCK to point at yubikey-agent.
+        '';
+      };
+
+      package = mkPackageOption pkgs "yubikey-agent" {};
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = [cfg.package];
+    systemd.packages = [cfg.package];
+
+    # This overrides the systemd user unit shipped with the
+    # yubikey-agent package
+    systemd.user.services.yubikey-agent = {
+      path = [pkgs.pinetry-curses];
+      wantedBy = ["default.target"];
+    };
+
+    # Yubikey-agent expects pcsd to be running in order to function.
+    services.pcscd.enable = true;
+
+    environment.extraInit = ''
+      if [ -z "$SSH_AUTH_SOCK" -a -n "$XDG_RUNTIME_DIR" ]; then
+        export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/yubikey-agent/yubikey-agent.sock"
+      fi
+    '';
+  };
+}
