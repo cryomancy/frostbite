25b762be3a2f2ea5aea9fc7fd6b39f7f2bed8f57
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-21 09:46:18 -0600

    add impermanence

--- modules/home/services/gpg-agent.nix
+++ modules/home/services/gpg-agent.nix
@@ -16,7 +16,13 @@ in {
     services = {
       gpg-agent = {
         enable = true;
+
+        # TODO: make an option for TUI e.g. pinentry-curses
         pinentryPackage = pkgs.pinentry-gnome3;
+
+        defaultCacheTtl = 60;
+        maxCacheTtl = 120;
+
         enableBashIntegration = true;
         enableFishIntegration = true;
         enableScDaemon = true;
