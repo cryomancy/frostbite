061f647212a7c05c1249b882613d6fa95c534411
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-21 09:45:10 -0600

    add impermanence

--- modules/home/development/virtualization.nix
+++ modules/home/development/virtualization.nix
@@ -2,6 +2,7 @@ scoped: {
   config,
   lib,
   pkgs,
+  user,
   ...
 }: let
   cfg = config.kosei.virtualization;
@@ -16,15 +17,22 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      distrobox
-      docker-compose
-      podman
-      podman-tui
-      skopeo
-      kubectl
-      kubevirt
-    ];
+    # TODO: add impermanence for distrobox and podman
+    persistence = lib.mkIf config.kosei.impermanence.enable {
+      "/nix/persistent/home/${user}" = {
+        directories = [".config/k9s"];
+      };
+
+      home.packages = with pkgs; [
+        distrobox
+        docker-compose
+        podman
+        podman-tui
+        skopeo
+        kubectl
+        kubevirt
+      ];
+    };
 
     programs.k9s.enable = true;
   };
