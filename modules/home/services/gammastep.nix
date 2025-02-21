8852238bd70e903a14e3a16e15b1576f05e76d0e
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-21 00:00:00 +0000

    add impermanence

--- modules/home/services/gammastep.nix
+++ modules/home/services/gammastep.nix
@@ -20,7 +20,15 @@ in {
     services = {
       gammastep = {
         enable = true;
+        enableVerboseLogging = true;
         provider = "geoclue2";
+        temperature = {
+          day = 6000;
+          night = 4600;
+        };
+        settings = {
+          general.adjustment-method = "wayland";
+        };
       };
     };
   };
