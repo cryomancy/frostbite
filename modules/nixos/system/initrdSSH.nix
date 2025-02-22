ade7c861dd838cf131b240e80a726a30dc4283dc
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-21 19:07:00 -0600

    remove redundant network attr from initrdSSH

--- modules/nixos/system/initrdSSH.nix
+++ modules/nixos/system/initrdSSH.nix
@@ -16,7 +16,7 @@ in {
 
   config = lib.mkIf cfg.enable {
     boot.initrd.network = {
-      network.enable = true;
+      enable = true;
       ssh.enable = true;
       # NOTE: everything else is inherited from
       # the SSH kosei module / NixOS modules
