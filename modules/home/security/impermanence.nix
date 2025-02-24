8bfb945189013ac71001a1a2498bff56ec34d6e1
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-24 00:00:00 +0000

    move imports under scope of config

--- modules/home/security/impermanence.nix
+++ modules/home/security/impermanence.nix
@@ -7,13 +7,13 @@ scoped: {
 }: let
   cfg = config.kosei.impermanence;
 in {
-  imports = [inputs.impermanence.homeManagerModules.impermanence];
-
   options = {
     kosei.impermanence.enable = lib.mkEnableOption "impermanence";
   };
 
   config = lib.mkIf cfg.enable {
+    imports = [inputs.impermanence.homeManagerModules.impermanence];
+
     home.persistence = {
       "/nix/persistent/home/${user}" = {
         allowOther = true;
