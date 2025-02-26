141b2ac121a187524b77598f8e86b9801a92cfb7
TahlonBrahic <tahlonbrahic@gmail.com>
2025-02-26 00:00:00 +0000

    add _module.args = user to home-manager in system template

--- lib/systemTemplate.nix
+++ lib/systemTemplate.nix
@@ -27,6 +27,7 @@ in
                 (builtins.attrNames kosei.modules.homeManager)
                 (module: builtins.getAttr module kosei.modules.homeManager);
               config.home.username = user;
+              config._module.args = user;
             });
           };
         }
