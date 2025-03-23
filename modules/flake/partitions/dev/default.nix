6cae581981edc9215dfc9e33ae3e0a990ba397f7
TahlonBrahic <tahlonbrahic@gmail.com>
2025-03-22 23:15:54 -0500

    add herclues-ci effects to dev partition

--- modules/flake/partitions/dev/default.nix
+++ modules/flake/partitions/dev/default.nix
@@ -1,13 +1,21 @@
-{inputs, ...}: {
-  systems = ["x86_64-linux"];
-
+{
+  self,
+  inputs,
+  ...
+}: let
+  projectRoot = self.outPath;
+in {
   imports = [
     inputs.devshell.flakeModule
     inputs.hercules-ci-effects.flakeModule
     inputs.git-hooks-nix.flakeModule
   ];
 
-  perSystem = {pkgs, ...}: {
+  perSystem = {
+    config,
+    pkgs,
+    ...
+  }: {
     devshells.default = {
       name = "Kosei Development Shell";
       devshell.prj_root_fallback = {
@@ -16,8 +24,9 @@
       };
 
       motd = ''
-        {117}❄ Kosei Developement Shell ❄{reset}
-        $(type -p menu &>/dev/null && menu)
+        ${config.pre-commit.installationScript}
+           {117}❄ Kosei Developement Shell ❄{reset}
+           $(type -p menu &>/dev/null && menu)
       '';
       env = [
         {
@@ -53,10 +62,82 @@
     };
 
     pre-commit = {
+      check.enable = true;
+
       settings = {
         addGcRoot = true;
+
+        excludes = [
+          "${projectRoot}/.direnv"
+          "${projectRoot}/.git"
+          "${projectRoot}/.local"
+        ];
+
         hooks = {
-          alejandra.enable = true;
+          alejandra = {
+            enable = true;
+          };
+
+          detect-aws-credentials = {
+            enable = true;
+          };
+
+          detect-private-keys = {
+            enable = true;
+          };
+
+          ripsecrets = {
+            enable = true;
+          };
+
+          sort-file-contents = {
+            enable = true;
+          };
+
+          treefmt = {
+            enable = true;
+            settings = {
+              no-cache = true;
+              fail-on-change = true;
+              formatters = [
+                pkgs.alejandra
+                pkgs.shfmt
+              ];
+            };
+          };
+
+          typos = {
+            enable = true;
+            settings = {
+              binary = false;
+              exclude = "*.nix";
+              diff = true;
+              hidden = false;
+              ignored-words = ["kosei" "Kosei"];
+              no-check-filenames = true;
+            };
+          };
+        };
+      };
+    };
+
+    hercules-ci = {
+      # Automatically updates flake inputs
+      flake-update = {
+        when = {
+          hour = [2];
+          dayOfMonth = builtins.genList (x: x) 31;
+        };
+
+        # This requires GitHub branch protection to be configured for the repository.
+        autoMergeMethod = true;
+        baseMerge.enable = true;
+        createPullRequest = true;
+        github-pages.branch = null; # Github pages deployment is maintained through Github CI
+
+        flakes = {
+          "." = {};
+          "./modules/flake/partitions/dev/" = {};
         };
       };
     };
--- modules/flake/partitions/dev/flake.nix
+++ modules/flake/partitions/dev/flake.nix
@@ -4,7 +4,11 @@
     devshell.url = "github:numtide/devshell";
     flake-parts.url = "github:hercules-ci/flake-parts";
     hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
-    git-hooks-nix.url = "github:cachix/git-hooks.nix";
+    git-hooks-nix = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    gitignore.url = "github:hercules-ci/gitignore.nix";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
