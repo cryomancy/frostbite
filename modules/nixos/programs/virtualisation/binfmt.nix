54c4a6fb1a52246da9b73e3841bfaa81479d6c92
TahlonBrahic <tahlonbrahic@gmail.com>
2025-03-30 20:51:51 -0500

    fix: remove unnecessary submodule

--- modules/nixos/programs/virtualisation/binfmt.nix
+++ modules/nixos/programs/virtualisation/binfmt.nix
@@ -6,13 +6,10 @@ _: {
   cfg = config.frostbite.virtualisation.binfmt;
 in {
   options = {
-    frostbite.virtualisation.binfmt = lib.mkOption {
-      type = lib.types.submodule {
-        enable = lib.mkOption {
-          default = true;
-          type = lib.types.bool;
-        };
-        options = {};
+    frostbite.virtualisation.binfmt = {
+      enable = lib.mkOption {
+        default = true;
+        type = lib.types.bool;
       };
     };
   };
