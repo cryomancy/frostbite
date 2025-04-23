dbbcb7754e0447fd2604bc0c869dca7e90a9d114
TahlonBrahic <tahlonbrahic@gmail.com>
2025-04-22 23:59:20 -0500

    update secrets module with new frostbite user options

--- modules/nixos/security/secrets/secrets.nix
+++ modules/nixos/security/secrets/secrets.nix
@@ -6,7 +6,7 @@ _: {
   ...
 }: let
   cfg = config.frostbite.security.secrets;
-  users = config.frostbite.users.accounts;
+  users = lib.attrsets.attrNames config.frostbite.users.users;
 in {
   imports = [inputs.sops-nix.nixosModules.sops];
 
