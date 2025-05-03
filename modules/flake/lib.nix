0a195edd92e57692cfa47187c03c3b8445431fb7
TahlonBrahic <tahlonbrahic@gmail.com>
2025-05-02 20:53:14 -0400

    fix: remove experimental pipes in place of traditional pipes

--- modules/flake/lib.nix
+++ modules/flake/lib.nix
@@ -1,36 +1,30 @@
-{inputs, ...}:
-let
-  inherit (inputs) eris;
-in
-{
+{inputs, ...}: let
+  inherit (inputs) eris nixpkgs;
+  inherit (nixpkgs) lib;
+  frostbiteLib = eris.lib.load {
+    src = ../../lib;
+    loader = eris.lib.loaders.scoped;
+  };
+in {
   config = {
     flake = {
-        lib =
-			  (eris.lib.load
-				{
-				  src = ../../lib;
-				  loader = eris.lib.loaders.scoped;
-				}
-			  )
-			  |>
-			  (inputs.nixpkgs.lib.attrsets.mapAttrsRecursiveCond
-				(x: builtins.isAttrs x)
-				(n: v:
-				  { lib =
-					{
-					  ${ (inputs.nixpkgs.lib.lists.last n) } = v;
-					};
-				  }
-				)
-			  )
-			  |>
-			  inputs.nixpkgs.lib.attrsets.collect (x: x ? lib)
-			  |>
-			  builtins.map (x: builtins.attrValues x)
-			  |>
-			  inputs.nixpkgs.lib.lists.flatten
-			  |>
-			  inputs.nixpkgs.lib.attrsets.mergeAttrsList;
-	    };
-	  };
+      lib = lib.pipe frostbiteLib [
+        (
+          inputs.nixpkgs.lib.attrsets.mapAttrsRecursiveCond
+          (x: builtins.isAttrs x)
+          (
+            n: v: {
+              lib = {
+                ${(inputs.nixpkgs.lib.lists.last n)} = v;
+              };
+            }
+          )
+        )
+        (inputs.nixpkgs.lib.attrsets.collect (x: x ? lib))
+        (builtins.map (x: builtins.attrValues x))
+        inputs.nixpkgs.lib.lists.flatten
+        inputs.nixpkgs.lib.attrsets.mergeAttrsList
+      ];
+    };
+  };
 }
