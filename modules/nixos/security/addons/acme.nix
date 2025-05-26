d1daac1652194e557fb6e91822008b168cb6ef91
TahlonBrahic <tahlonbrahic@gmail.com>
2025-05-26 17:13:28 -0400

    update: add aws option that ties into sops-nix for ACME

--- modules/nixos/security/addons/acme.nix
+++ modules/nixos/security/addons/acme.nix
@@ -29,6 +29,11 @@ in {
         type = lib.types.str;
         default = null;
       };
+
+      useAWS = lib.mkOption {
+        type = lib.types.bool;
+        default = false;
+      };
     };
   };
 
@@ -47,7 +52,7 @@ in {
       defaults = {
         server = "https://acme-v02.api.letsencrypt.org/directory";
 
-        dnsProvider = "route53";
+        dnsProvider = lib.mkIf cfg.useDNS "route53";
         dnsResolver = "8.8.8.8:53";
 
         environmentFile = "${config.sops.templates."certs.secret".path}";
@@ -66,7 +71,9 @@ in {
       };
     };
 
-    sops.secrets = {
+    # These are used to access AWS for DNS validation
+    # Must have a key:value pair for each of these
+    sops.secrets = lib.mkIf cfg.useAWS {
       "AWS_ACCESS_KEY_ID" = {};
       "AWS_SECRET_ACCESS_KEY" = {};
 
