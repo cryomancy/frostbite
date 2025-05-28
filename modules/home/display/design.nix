222b06f9517ec07d1cb5126b946a37ba0b8d6df2
TahlonBrahic <tahlonbrahic@gmail.com>
2025-05-27 21:51:25 -0400

    update lockfile

--- modules/home/display/design.nix
+++ modules/home/display/design.nix
@@ -43,7 +43,7 @@ in {
         gtk.enable = true;
         ghostty.enable = true;
         hyprland.enable = true;
-        #hyprlock.enable = true;
+        hyprlock.enable = true;
         hyprpaper.enable = false;
         kitty.enable = true;
         librewolf.enable = true;
