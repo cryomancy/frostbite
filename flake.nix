0a525633d9a54dbbc2287c8fb95ababa040bc5c2
TahlonBrahic <tahlonbrahic@gmail.com>
2025-07-07 21:26:09 -0400

    update inputs

--- flake.nix
+++ flake.nix
@@ -80,7 +80,7 @@
     impermanence = {
       url = "github:nix-community/impermanence";
     };
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-droid.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-stable.url = "github:/nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:/nixos/nixpkgs/nixpkgs-unstable";
