35e062e9755194449afe312bf1b163d3cafb2011
TahlonBrahic <tahlonbrahic@gmail.com>
2025-07-07 21:49:18 -0400

    update inputs for proper stylix 25.05 release

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix = {
-      url = "github:danth/stylix";
+      url = "github:danth/stylix/release-25.05";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         home-manager.follows = "home-manager";
