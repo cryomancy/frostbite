62f8e5c6205b4d8ea9569369c7228fa26f47bb35
TahlonBrahic <tahlonbrahic@gmail.com>
2025-11-20 18:10:52 -0500

    feat: add comma, nix-index, and nix-index database

--- modules/home/utils/nix.nix
+++ modules/home/utils/nix.nix
@@ -1,11 +1,14 @@
-_: {
+_:
+{
   config,
   lib,
   pkgs,
   ...
-}: let
+}:
+let
   cfg = config.frostbite.utils.nix;
-in {
+in
+{
   options = {
     frostbite.utils.nix = {
       enable = lib.mkOption {
@@ -16,6 +19,14 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
+    programs.nix-index-database.comma.enable = true;
+    programs.nix-index = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+      enableFishIntegration = true;
+    };
+
     home.packages = with pkgs; [
       nix-du # Tool to determine which gc-roots take space in your nix store
       nix-init # Generate Nix packages from URLs
