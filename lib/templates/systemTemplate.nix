b00653f994656287b3572bea7425f1215efe6fa9
TahlonBrahic <tahlonbrahic@gmail.com>
2025-11-20 18:02:24 -0500

    Remove antipattern that prevents overlays and other config.nixpkgs.config options work

--- lib/templates/systemTemplate.nix
+++ lib/templates/systemTemplate.nix
@@ -1,41 +1,45 @@
-_: {
-  extraModules ? [],
+_:
+{
+  extraModules ? [ ],
   inputs,
   system,
   outPath,
   ...
-}: let
+}:
+let
   inherit (inputs) frostbite home-manager nixpkgs;
-  specialArgs = {inherit inputs system outPath;};
+  specialArgs = { inherit inputs system outPath; };
 in
-  nixpkgs.lib.nixosSystem {
-    pkgs = inputs.frostbite.legacyPackages.${system};
-    inherit specialArgs;
-    modules =
-      [
-        home-manager.nixosModules.home-manager
-        ({config, ...}: {
-          home-manager = {
-            backupFileExtension = "bak";
-            extraSpecialArgs = specialArgs;
-            useGlobalPkgs = true; # Use system nixpkgs, remove impure dependencies
-            useUserPackages = true; # Installs packages to /etc/profiles
-            # Iterates over a list of users provided in the function call
-            users =
-              nixpkgs.lib.attrsets.genAttrs (nixpkgs.lib.attrsets.attrNames config.frostbite.users.users)
+nixpkgs.lib.nixosSystem {
+  # !!! ANTIPATTERN !!!
+  # pkgs = inputs.frostbite.legacyPackages.${system};
+  inherit specialArgs;
+  modules = [
+    home-manager.nixosModules.home-manager
+    (
+      { config, ... }:
+      {
+        home-manager = {
+          backupFileExtension = "bak";
+          extraSpecialArgs = specialArgs;
+          useGlobalPkgs = true; # Use system nixpkgs, remove impure dependencies
+          useUserPackages = true; # Installs packages to /etc/profiles
+          # Iterates over a list of users provided in the function call
+          users =
+            nixpkgs.lib.attrsets.genAttrs (nixpkgs.lib.attrsets.attrNames config.frostbite.users.users)
               (user: {
-                imports =
-                  nixpkgs.lib.forEach
-                  (builtins.attrNames frostbite.modules.homeManager)
-                  (module: builtins.getAttr module frostbite.modules.homeManager);
+                imports = nixpkgs.lib.forEach (builtins.attrNames frostbite.modules.homeManager) (
+                  module: builtins.getAttr module frostbite.modules.homeManager
+                );
                 config.home.username = user;
-                config._module.args = {inherit user;};
+                config._module.args = { inherit user; };
               });
-          };
-        })
-      ]
-      ++ extraModules
-      ++ nixpkgs.lib.forEach
-      (builtins.attrNames frostbite.modules.nixos)
-      (module: builtins.getAttr module frostbite.modules.nixos);
-  }
+        };
+      }
+    )
+  ]
+  ++ extraModules
+  ++ nixpkgs.lib.forEach (builtins.attrNames frostbite.modules.nixos) (
+    module: builtins.getAttr module frostbite.modules.nixos
+  );
+}
